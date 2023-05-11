ca48252fc80b18cc3c29bed7c91af98c34ca636b
Jesse Pararadero <72135199+jesse345@users.noreply.github.com>
2023-05-11 10:49:04 +0800

    ew

---
+++ Model/db.php
@@ -0,0 +1,60 @@
+<?php
+
+
+$conn;
+
+function connect(){
+    global $conn;
+    $conn = mysqli_connect("localhost","root","","hackathon");
+
+    return $conn;
+}
+
+function getAll($tablename){
+    global $conn;
+    connect();
+    $query = mysqli_query($conn,"SELECT * FROM `$tablename`");
+    disconnect();
+    return $query;
+}
+
+function getRecord($tablename,$fld,$val){
+    global $conn;
+    connect();
+    $query = mysqli_query($conn,"SELECT * FROM `$tablename` WHERE `$fld` = $val");
+    disconnect();
+    return $query;
+}
+
+
+function insertRecord($tablename,$fields,$values){
+
+    global $conn;
+    connect();
+    $flds = implode("`,`",$fields);
+    $vals = implode("','",$values);
+    $query = mysqli_query($conn,"INSERT INTO `$tablename` (`$flds`) VALUES ('$vals')");
+    disconnect();
+}
+
+function editRecord($tablename,$fields,$values){
+    global $conn;
+    connect();
+    for($i = 1 ; $i < count($fields); $i++){
+        $query = mysqli_query($conn,"UPDATE `$tablename` SET `$fields[$i]` = '$values[$i]' WHERE `$fields[0]` = '$values[0]'");
+    }
+    disconnect();
+}
+
+function deleteRecord($tablename,$fields,$values){
+    global $conn;
+    connect();
+    $query = mysqli_query($conn,"DELETE FROM `$tablename` WHERE `$fld` = $val");
+    disconnect();
+}
+
+function disconnect(){
+    global $conn;
+    mysqli_close($conn);
+}
+
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+include 'Model/db.php';
+
+
+
+// insertRecord('hello',array('id','name','lastname'),array(1,'klient','manlangit'));
+
