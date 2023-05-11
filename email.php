86ef2a8aa471c152f4bcac25b5ddc53e59aa06e3
Jesse Pararadero <72135199+jesse345@users.noreply.github.com>
2023-05-11 10:17:42 +0800

    SDA

---
+++ email.php
@@ -0,0 +1,50 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// For login
+
+function sendemail()
+{
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+        $mail->isSMTP(); //Send using SMTP
+        $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
+        $mail->SMTPAuth = true; //Enable SMTP authentication
+        $mail->Username = 'eaquariaofficial@gmail.com'; //SMTP username
+        $mail->Password = 'fczbhjmbpmfcxdfm'; //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+        $mail->Port = 465;
+
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+
+        //Recipients
+        $mail->setFrom('eaquariaofficial@gmail.com', 'E-Aquaria Official');
+        $mail->addAddress("lourenceklient7@gmail.com","Klient"); //Add a recipient
+
+        //Content
+        $mail->isHTML(true); //Set email format to HTML
+
+        $rand = rand();
+        $mail->Subject = 'E-Aquaria Verification Code';
+        $mail->Body = "This is your verification code <b>12345</b> <br>This will expire in 2 minutes";
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+
+sendemail();
